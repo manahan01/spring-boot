dc2344d6ae2262fd6c588b1f21048e592abbc472
manahan01 <manahanlubis01@gmail.com>
2022-12-10 19:55:30 +0700

    First API

---
+++ src/main/java/com/manahan/main.java
@@ -0,0 +1,18 @@
+package com.manahan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class main {
+    public static void main(String[] args) {
+        SpringApplication.run(main.class, args);
+    }
+    @GetMapping("/greet")
+    public String greet(){
+        return "hello";
+    }
+}
