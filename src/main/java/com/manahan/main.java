c3540eb3fba08291906b67ea75670f6a75ece0fe
manahan01 <manahanlubis01@gmail.com>
2022-12-10 20:32:41 +0700

    Spring Web MVC, @RestControler dan @GetMapping(read(CRUD)) @PostMapping(write(CRUD)) @PutMapping(update(CRUD)) @DeleteMapping(delete(CRUD))

--- src/main/java/com/manahan/main.java
+++ src/main/java/com/manahan/main.java
@@ -1,24 +1,19 @@
 package com.manahan;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@SpringBootApplication
-@ComponentScan(basePackages = "com.manahan")
-@EnableAutoConfiguration
-@Configuration
+@SpringBootApplication
 @RestController
 public class main {
     public static void main(String[] args) {
         SpringApplication.run(main.class, args);
     }
     @GetMapping("/greet")
-    public String greet(){
-        return "hello";
+    public GreetResponse greet(){
+        return new GreetResponse("Hello");
     }
+    record GreetResponse(String greet){}
 }
